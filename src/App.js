c54b544b6572ee75bfc677c78566a50da037798f
christy313 <christycreativity@gmail.com>
2022-09-18 22:10:45 -0400

    Update cover imgae

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./index.css";
 import CardList from "./CardList";
 
 const App = () => (
-  <div className="tc">
+  <main className="tc">
     <div className="cover-image">
-      <img src="./images/cover.jpg" alt="cover" />
+      <img
+        src="https://christy313.github.io/react15-showcase/images/react-logo.jpg"
+        alt="cover"
+      />
       <div className="intro">
         <h1>React projects</h1>
         <p>Projects are the best way to dive into the skills</p>
       </div>
     </div>
     <CardList />
-  </div>
+  </main>
 );
 
 export default App;
