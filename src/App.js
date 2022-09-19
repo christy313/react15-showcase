d3f3fd89df8561b1edf9d83659d8e5af9d5b37d5
christy313 <christycreativity@gmail.com>
2022-09-18 22:27:41 -0400

    Add react icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import { FaReact } from "react-icons/fa";
 import "./index.css";
 import CardList from "./CardList";
 
 const App = () => (
   <main className="tc">
-    <div className="cover-image">
-      <img
-        src="https://christy313.github.io/react15-showcase/images/react-logo.jpg"
-        alt="cover"
-      />
+    <div className="cover">
+      <FaReact className="react-icon" />
       <div className="intro">
         <h1>React projects</h1>
         <p>Projects are the best way to dive into the skills</p>
