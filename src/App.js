5729e3d6d0d47a0f292de73dac74c0957a0dd934
christy313 <christycreativity@gmail.com>
2022-09-18 21:35:53 -0400

    Move images folder in public folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./index.css";
-import cover from "./images/cover.jpg";
-import CardList from "./components/CardList";
+import CardList from "./CardList";
 
 const App = () => (
   <div className="tc">
     <div className="cover-image">
-      <img src={cover} alt="cover" />
+      <img src="/images/cover.jpg" alt="cover" />
       <div className="intro">
         <h1>React projects</h1>
         <p>Projects are the best way to dive into the skills</p>
